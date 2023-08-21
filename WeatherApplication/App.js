0d7b428e2323cf790f7421d8d01a9ba8fb73dab1
Kitkamseng <64192497+Kitkamseng@users.noreply.github.com>
2023-08-21 16:25:04 +0800

    Basic Home Screen Done

--- WeatherApplication/App.js
+++ WeatherApplication/App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  SafeAreaView 
+} from 'react-native';
+import styles from '../WeatherApplication/styles/commonStyles';
+import HomePage from './screen/HomePage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView 
+      style={styles.container}
+    >
+      <View>
+        <HomePage />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
