9393203b6ff11ed15be0b895383e7ac171b80741
Kitkamseng <64192497+Kitkamseng@users.noreply.github.com>
2023-08-22 16:05:30 +0800

    Add the selection tab with the logic included

---
+++ WeatherApplication/styles/HomePageStyles.js
@@ -0,0 +1,43 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const HomePageStyles = StyleSheet.create({
+
+    mainContainer: {
+        flex: 1, 
+        paddingHorizontal: 10, 
+        justifyContent: 'center',
+      },
+      listTab: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        marginBottom: 20,
+      },    
+      btnTab: {
+        width: Dimensions.get('window').width / 2.5, 
+        flexDirection: 'row',
+        borderWidth: 0.5, 
+        borderColor: '#EBEBEB', 
+        borderRadius: 20,
+        padding: 10, 
+        margin: 10,
+        marginTop: 60, 
+        justifyContent: 'center',
+      },
+      textTab: {
+        fontSize: 16, 
+        color: 'white',
+      },
+      btnTabActive: {
+        backgroundColor: '#D3D3D3',
+      },
+      textTabActive: {
+        color: '#fff',
+      },
+      itemContainer: {
+        flexDirection: 'row',
+        paddingVertical: 15, 
+      }
+      
+});
+
+export default HomePageStyles;
